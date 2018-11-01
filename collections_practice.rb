0f9f16654aa6a225601b20b07b72cf9ddafe94b7
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-01 06:21:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
 # your code goes here
+require "pry"
+def begins_with_r(array)
+  array.all?{|el| el[0] == "r"}
+end 
+
+def contain_a(array)
+  array.select{|el| el.include?("a")}
+end 
+
+def first_wa (array)
+  array.each{|el| return el if el[0..1] == "wa"}
+end 
+
+def remove_non_strings(array)
+  array.select{|el| el.is_a?(String)}
+end 
+
+def count_elements(elements)
+  uniq = elements.uniq 
+  array = []
+  
+  uniq.each do |el|
+    count = 0  
+    elements.each do |name|
+      if el == name 
+        count += 1
+      end 
+    end 
+    hash = el 
+    hash[:count] = count 
+    array << hash 
+  end 
+  array
+end 
+
+def merge_data(keys, data)
+  array = []
+  data.each do |key, value| 
+    key.each do |name, name_data| 
+      keys.each do |k, v|
+        if k.values == [name] 
+          array << name_data.merge(k)
+        end 
+      end 
+    end 
+  end 
+  array 
+end 
+
+def find_cool(cool)
+  cool.select {|name_hash| name_hash[:temperature] == "cool"}
+end 
+
+def organize_schools(schools)
+  schools.each_with_object({}) do  |(name, data), result|
+    (result[data[:location]] ||= []) << name
+  end 
+end 
