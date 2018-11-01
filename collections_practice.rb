1e5aafab609e2b7d725408a762e3ed2268742c82
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-01 05:39:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 # your code goes here
+require "pry"
+def begins_with_r(array)
+  array.all?{|el| el[0] == "r"}
+end 
+
+def contain_a(array)
+  array.select{|el| el.include?("a")}
+end 
+
+def first_wa (array)
+  array.each{|el| return el if el[0..1] == "wa"}
+end 
+
+def remove_non_strings(array)
+  array.select{|el| el.is_a?(String)}
+end 
+
+def count_elements(elements)
+  uniq = elements.uniq 
+  array = []
+  
+  uniq.each do |el|
+    count = 0  
+    elements.each do |name|
+      if el == name 
+        count += 1
+      end 
+    end 
+    hash = el 
+    hash[:count] = count 
+    array << hash 
+  end 
+  array
+end 
+
+def merge_data(keys, data)
+  p data 
+  p keys 
+  data.each do |key, value| 
+    key.each do |k, v| 
+      
+    end 
+  end 
+end 
+
+def find_cool(cool)
+  
+end 
+
+def organize_schools(schools)
+  
+end 
+
